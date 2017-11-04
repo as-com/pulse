0975fb8378c677ca5be7072ddf8d9760fec080a7
Harshal Sheth <hsheth2@gmail.com>
2017-11-04 14:47:28 -0400

    fix word aggr

--- backend/aggregators/Words.ts
+++ backend/aggregators/Words.ts
@@ -1,26 +1,34 @@
 import {Aggregator} from "../Aggregator";
 import {Post} from "../Ingester";
 import {MinuteBuckets} from "../MinuteBuckets";
+import * as _ from "lodash";
 
 const blacklist = new Set(require("./word_blacklist.json").map(w => w.toLowerCase()));
 
 export class Words extends Aggregator {
-	buckets = new MinuteBuckets<string>(15000);
+	buckets = new MinuteBuckets<string>(10000);
 
 	_process(post: Post) {
 		const matches = post.message.match(/\b(\w+)\b/g); // TODO: handle non-english
 		if (matches === null) {
 			return;
 		}
+		let exclude = post.mentions.concat(post.hashtags).map(w => w.toLowerCase());
 		const words = matches.map(x => x.toLowerCase()).filter(w => {
 			if (w.length <= 2) {
 				return false;
 			}
-
+			// if (w === "kiwi") {
+			// 	console.log(post);
+			// }
+			for (let i = 0; i < exclude.length; i++) {
+				if (w === exclude[i])
+					return false;
+			}
 			return !blacklist.has(w);
 		});
 		const date = new Date();
-		words.forEach(w => this.buckets.push(/*post.time*/ date, w));
+		_.uniq(words).forEach(w => this.buckets.push(/*post.time*/ date, w));
 	}
 
 	calc() {
