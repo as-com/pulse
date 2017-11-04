0f15edb0949bb068e263ab64a3e8db173df3b830
Andrew Sun <me@andrewsun.com>
2017-11-04 17:40:14 -0400

    gah

--- frontend/public/scripts/emojis.js
+++ frontend/public/scripts/emojis.js
@@ -2,5 +2,15 @@ const emojisApp = new Vue({
 	el: "#emojis",
 	data: {
 		items: []
-	}
+	},
+
 });
+
+socket.on("emoji", function (data) {
+	// emojisApp.items.unshift(data[0]);
+	emojisApp.items = data;
+});
+
+setInterval(function() {
+	emojisApp.items.shift()
+}, 300);
