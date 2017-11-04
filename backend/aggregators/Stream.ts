ba72bccbc5b64d4a0d8dc2eb7aa8833ae4cb3daf
Harshal Sheth <hsheth2@gmail.com>
2017-11-04 17:22:30 -0400

    Double stream rate

--- backend/aggregators/Stream.ts
+++ backend/aggregators/Stream.ts
@@ -2,6 +2,9 @@ import {Aggregator} from "../Aggregator";
 import {Post} from "../Ingester";
 import * as _ from 'lodash';
 
+const tick = 500;
+const perSecond = 4;
+
 export class Stream extends Aggregator {
     private queue : Post[] = [];
 
@@ -12,13 +15,14 @@ export class Stream extends Aggregator {
     calc() {
         // randomly pick one
         const qu = _.shuffle(this.queue);
-        if (qu.length > 2) {
-            const chosen = qu[1];
-
-            setTimeout(() => { this.broadcast(qu[0])}, 250);
-
-            return chosen;
+        if (qu.length > perSecond) {
+            for (let i = 0; i < perSecond; i++) {
+                const chosen = qu[i];
+                const time = i * tick / perSecond;
+                setTimeout(() => { this.broadcast(chosen)}, time);
+            }
         }
+        this.queue = [];
         return null;
     }
 
